86aa44f6259bd476536393bc02d64239364d80cd
hyusuf4 <hyusuf63@gmail.com>
2019-03-19 17:32:43 -0600

    Update api.py

--- api/api.py
+++ api/api.py
@@ -21,13 +21,13 @@ class RegisterAPI(generics.GenericAPIView):
             "token":AuthToken.objects.create(user),
             "isActive":user.is_active
         })
-    @receiver(pre_save, sender=User)
-    def set_new_user_inactive(sender, instance, **kwargs):
-        if instance._state.adding is True:
-            print("Creating Inactive User")
-            instance.is_active = False
-        else:
-            print("Updating User Record")
+#     @receiver(pre_save, sender=User)
+#     def set_new_user_inactive(sender, instance, **kwargs):
+#         if instance._state.adding is True:
+#             print("Creating Inactive User")
+#             instance.is_active = False
+#         else:
+#             print("Updating User Record")
 class LoginAPI(generics.GenericAPIView):
     serializer_class=LoginSerializer
 
