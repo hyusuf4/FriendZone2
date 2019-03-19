d4a4f178150b85b319b6be4e70b6fc519571de94
Hamdi Yusuf <hyusuf63@gmail.com>
2019-03-19 12:24:57 -0600

    update

--- api/urls.py
+++ api/urls.py
@@ -8,45 +8,43 @@ from .views import ListAuthors,AuthorDetails,PostOfAuth,PublicPosts,PostOfAuthor
 
 urlpatterns = [
     #Just retrieves list of authors in the database
-    path('api/auth/',include('knox.urls')),
+    path('/auth/',include('knox.urls')),
     #for registering a user route
-    path('api/auth/register/',RegisterAPI.as_view()),
+    path('auth/register',RegisterAPI.as_view()),
 
     # for logining in a user route
-    path('api/auth/login/',LoginAPI.as_view()),
+    path('auth/login',LoginAPI.as_view()),
 
-    path('api/auth/user/',UserAPI.as_view()),
+    path('auth/user',UserAPI.as_view()),
 
-    path('api/auth/logout/',knox_views.LogoutView.as_view(),name='knox_logout'),
+    path('auth/logout',knox_views.LogoutView.as_view(),name='knox_logout'),
 
-    path('api/authors/', ListAuthors.as_view()),
+    path('authors/', ListAuthors.as_view()),
     #Retrieves this specific authors details
-    path('api/authors/<pk>/', AuthorDetails.as_view(),name='authors'),
+    path('authors/<pk>/', AuthorDetails.as_view(),name='authors'),
 
-    path('api/author/posts/',PostOfAuth.as_view()),
+    path('author/posts/',PostOfAuth.as_view()),
 
 
     #All posts marked as public on the server
-    path('api/posts/',PublicPosts.as_view()),
+    path('posts/',PublicPosts.as_view()),
     #All posts made by this specific author, visible to authenticated user
-    path('api/author/<pk>/posts/', PostOfAuthors.as_view()),
+    path('author/<pk>/posts/', PostOfAuthors.as_view()),
     #Just retrieves a single post with that id
-    path('api/posts/<pk>/',PostDetails.as_view()),
+    path('posts/<pk>/',PostDetails.as_view()),
     #Just retrieves comments to that specific post
-    path('api/posts/<pk>/comments/',PostComments.as_view()),
+    path('posts/<pk>/comments/',PostComments.as_view()),
     #Just sends a friend request
-    path('api/friendrequests/',views.send_friend_request),
+    path('friendRequest/',views.send_friend_request),
     # result of a friend request
-    path('api/friendresult/', views.respond_to_friend_request),
+    path('friendResult', views.respond_to_friend_request),
     # Just un-befriend(unfollow) an author
-    path('api/unfriend/', views.unfriend),
+    path('unfriend/', views.unfriend),
     # Get all my friends
-    path('api/authors/<authorid>/friends/', views.get_friends),
+    path('friends/', views.get_friends),
     # Get all my friends
-    path('api/local_friends/', views.get_friends_local),
-    # Ask if 2 authors are friends
-    path('api/author/<authorid>/friends/<authorid2>/', views.check_friendship)
+    path('local_friends/', views.get_friends_local)
 
 ]
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+
