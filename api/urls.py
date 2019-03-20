aad41e4709509c610740cc4a30391db298737442
Yuqi Zhang <yuqi8@ualberta.ca>
2019-03-19 19:56:17 -0600

    names for paths

--- api/urls.py
+++ api/urls.py
@@ -14,39 +14,39 @@ urlpatterns = [
     # for logining in a user route
     path('auth/login',LoginAPI.as_view(), name='login'),
 
-    path('auth/user',UserAPI.as_view()),
+    path('auth/user',UserAPI.as_view(), name='user'),
 
     path('auth/logout',knox_views.LogoutView.as_view(),name='knox_logout'),
 
-    path('authors/', ListAuthors.as_view()),
+    path('authors/', ListAuthors.as_view(), name='authors_list'),
     #Retrieves this specific authors details
     path('authors/<pk>/', AuthorDetails.as_view(),name='authors'),
 
-    path('author/posts/',PostOfAuth.as_view()),
+    path('author/posts/',PostOfAuth.as_view(), "auth_posts"),
 
 
     #All posts marked as public on the server
     path('posts/',PublicPosts.as_view(), name='posts'),
     #All posts made by this specific author, visible to authenticated user
-    path('author/<pk>/posts/', PostOfAuthors.as_view()),
+    path('author/<pk>/posts/', PostOfAuthors.as_view(), name='author_posts'),
     #Just retrieves a single post with that id
-    path('posts/<pk>/',PostDetails.as_view()),
+    path('posts/<pk>/',PostDetails.as_view(), name="post_info"),
     #Just retrieves comments to that specific post
-    path('posts/<pk>/comments/',PostComments.as_view()),
+    path('posts/<pk>/comments/',PostComments.as_view(), name="post_comments"),
     #Just sends a friend request
-    path('friendRequest/',views.send_friend_request),
+    path('friendRequest/',views.send_friend_request, name="friend_request"),
     # result of a friend request
-    path('friendResult', views.respond_to_friend_request),
+    path('friendResult', views.respond_to_friend_request, name="respond_frequest"),
     # Just un-befriend(unfollow) an author
-    path('unfriend/', views.unfriend),
-    # Get all my friends
-    path('friends/', views.get_friends),
+    path('unfriend/', views.unfriend, name="unbefriend"),
+    # # Get all my friends
+    # path('friends/', views.get_friends, name="get"),
     # Get all my friends
     path('local_friends/', views.get_friends_local),
     # Get all my friends
-    path('api/authors/<authorid>/friends/', views.get_friends),
+    path('api/authors/<authorid>/friends/', views.get_friends, name="get_global_friends"),
     # Get all my friends
-    path('api/local_friends/', views.get_friends_local),
+    path('api/uthors/<authorid>/local_friends/', views.get_friends_local, name="get_local_friends"),
     # Ask if 2 authors are friends
     path('api/author/<authorid>/friends/<authorid2>/', views.check_friendship, name='friendship')
 ]
