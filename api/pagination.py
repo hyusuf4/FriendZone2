5a055cb94aeda075d228c024db985b61974fa36f
hyusuf4 <hyusuf63@gmail.com>
2019-03-26 17:52:53 -0600

    Update pagination.py

--- api/pagination.py
+++ api/pagination.py
@@ -19,7 +19,7 @@ class CustomPagination(PageNumberPagination):
         return Response(response)
 
 class CommentPagination(PageNumberPagination):
-
+    page_size=50
     page_size_query_param = 'size'
     max_page_size = 50
 
